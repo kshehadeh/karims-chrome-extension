c17f7d0dc341ec3eded6e969d42c95d8daa32ce8
quarterback-action <quarterback-no-reply@underarmour.com>
2025-01-29 08:11:51 -0500

    Added command to clear old tabs and fixed issue with options

--- src/background/features/tab-closer.js
+++ src/background/features/tab-closer.js
@@ -1,9 +1,13 @@
 const MAX_TAB_AGE = 12 * 60 * 60 * 1000; // 24 hours in milliseconds
 
+export async function getOptions() {
+    return await chrome.storage.sync.get(["tabAge", "autoCloseTabsToggle"]);    
+}
+
 export async function setupAutoTabCloser() {
     console.log("[Karim's Extension] Setting up auto tab closer");
 
-    const {tabAge,autoCloseTabsToggle} = await chrome.storage.sync.get(["tabAge","autoCloseTabsToggle"]) || "12";
+    const {tabAge,autoCloseTabsToggle} = await getOptions()
 
     if (!autoCloseTabsToggle) {
         console.log("[Karim's Extension] Auto close tabs is disabled");
@@ -24,6 +28,8 @@ export async function setupAutoTabCloser() {
 }
 
 export function closeOldTabs(ageOfTabsInMilliseconds = MAX_TAB_AGE) {
+    console.log(`[Karim's Extension] Closing tabs older than ${ageOfTabsInMilliseconds} milliseconds`);
+
     chrome.tabs.query({}, (tabs) => {
         const now = Date.now();
         tabs.forEach((tab) => {
@@ -46,4 +52,12 @@ export default function setup() {
     chrome.runtime.onStartup.addListener(() => {
         setupAutoTabCloser();
     });    
+
+    chrome.commands.onCommand.addListener(async (command) => {        
+        if (command === "close_old_tabs") {
+            const {tabAge} = await getOptions()
+            const tabAgeInMilleseconds = parseInt(tabAge, 10) * 60 * 60 * 1000
+            closeOldTabs(tabAgeInMilleseconds)
+        }
+    });
 }
