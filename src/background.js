23788537b11bc1451595360c802d462940a5c3a8
quarterback-action <quarterback-no-reply@underarmour.com>
2025-01-29 12:51:51 -0500

    Removed sidebar stuff

--- src/background.js
+++ src/background.js
@@ -1,8 +1,6 @@
 import tabCloser from "./background/features/tab-closer.js";
 import copyToClipboard from "./background/features/copy-url.js";
-import sidebar from "./background/features/sidebar/index.js";
 
 tabCloser();
 copyToClipboard();
-sidebar();
 
